0c99899e23219e0758b993f6e3d1b861c615e462
Renan Gomes <renanonline8@gmail.com>
2017-08-01 16:54:14 -0300

    Primeiro Commit

---
+++ loja/altera-produto.php
@@ -0,0 +1,31 @@
+<?php
+require_once("cabecalho.php");
+require_once("banco-produto.php");
+require_once("class/Produto.php");
+
+$produto = new Produto();
+$produto->id = $_POST['id'];
+$produto->nome = $_POST['nome'];
+$produto->preco = $_POST['preco'];
+$produto->descricao = $_POST['descricao'];
+
+if(array_key_exists('usado', $_POST)) {
+	$produto->usado = "true";
+} else {
+	$produto->usado = "false";
+}
+
+$produto->categoria_id = $_POST['categoria_id'];
+
+if(alteraProduto($conexao, $produto)) { ?>
+	<p class="text-success">O produto <?= $produto->nome ?>, <?= $produto->preco ?> foi alterado.</p>
+<?php 
+} else {
+	$msg = mysqli_error($conexao);
+?>
+	<p class="text-danger">O produto <?= $produto->nome ?> não foi alterado: <?= $msg?></p>
+<?php
+}
+?>
+
+<?php include("rodape.php"); ?>
---
+++ loja/banco-produto.php
@@ -0,0 +1,48 @@
+<?php
+require_once("conecta.php");
+
+function listaProdutos($conexao) {
+
+	$produtos = array();
+	$resultado = mysqli_query($conexao, "select p.*,c.nome as categoria_nome from 
+			produtos as p join categorias as c on c.id=p.categoria_id");
+
+	while($produto = mysqli_fetch_assoc($resultado)) {
+		array_push($produtos, $produto);
+	}
+
+	return $produtos;
+}
+
+function insereProduto($conexao, Produto $produto) {
+
+	$query = "insert into produtos (nome, preco, descricao, categoria_id, usado) 
+		values ('{$produto->nome}', {$produto->preco}, '{$produto->descricao}', {$produto->categoria_id}, {$produto->usado})";
+
+	return mysqli_query($conexao, $query);
+}
+
+function alteraProduto($conexao, Produto $produto) {
+
+	$query = "update produtos set nome = '{$produto->nome}', preco = {$produto->preco}, 
+		descricao = '{$produto->descricao}', categoria_id= {$produto->categoria_id}, 
+			usado = {$produto->usado} where id = '{$produto->id}'";
+
+	return mysqli_query($conexao, $query);
+}
+
+function buscaProduto($conexao, $id) {
+
+	$query = "select * from produtos where id = {$id}";
+	$resultado = mysqli_query($conexao, $query);
+	$produto = mysqli_fetch_assoc($resultado);
+
+	return $produto;
+}
+
+function removeProduto($conexao, $id) {
+
+	$query = "delete from produtos where id = {$id}";
+
+	return mysqli_query($conexao, $query);
+}
